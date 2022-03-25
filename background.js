7c4e1e9826f08ef61d209261027c7f514719c0d9
Serhii <tatsiak@gmail.com>
2022-03-25 17:22:48 +0100

    Remove logs

--- background.js
+++ background.js
@@ -1,11 +1,8 @@
-console.log("run");
 chrome.webRequest.onHeadersReceived.addListener(
   function (details) {
-    console.log("hreaders");
     const responseHeaders = details.responseHeaders;
     responseHeaders.map((header) => {
       if (header.name === "set-cookie") {
-        console.log("working");
         header.value = header.value.replace(
           "samesite=lax",
           "SameSite=None; Secure"
